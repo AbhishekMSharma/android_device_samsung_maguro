3c4d55691b44699b9e1ace4a43c216cd52dd8622
nocoast <voteforpedro8@gmail.com>
2012-09-13 01:34:59 -0500

    Update kernel custom toolcahin cflag for linaro.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,3 +22,5 @@ TARGET_BOARD_INFO_FILE ?= device/samsung/maguro/board-info.txt
 # Use the part that is common between all tunas
 include device/samsung/tuna/BoardConfig.mk
 
+# Kernel
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7.2
